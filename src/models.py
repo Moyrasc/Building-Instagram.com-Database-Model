9415088213ed99b01d99d726282d72260514a106
Moyra_sc <noemisolac@gmail.com>
2022-08-12 19:41:34 +0000

    finish data model

--- src/models.py
+++ src/models.py
@@ -8,23 +8,43 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(20), unique=True, nullable=False)
+    email = Column(String(30), unique=True, nullable=False)
+    password= Column(Integer, unique=False, nullable=False)
+    posts = relationship('post', backref='user', lazy=True)
+    friends = relationship('friend', backref='user', lazy=True)
+    followers = relationship('followers', backref='user', lazy=True)
+    my_saved= relationship('saved', backref='user', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    title = Column(String(100),unique=False, nullable=False)
+    text = Column(String(300),unique=False, nullable=False)
+    likes = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    saved_post = relationship('saved', backref='post',lazy=True)
+
+class Friend(Base):
+    __tablename__ = 'friends'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    friend_id = Column(Integer, ForeignKey('user.id'))
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    follower_id = Column(Integer, ForeignKey('user.id'))
+
+class Saved(Base):
+    __tablename__ = 'saved'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
     def to_dict(self):
         return {}
